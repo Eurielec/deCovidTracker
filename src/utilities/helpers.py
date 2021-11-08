77c7e3bdfee3c890b3d7d0d917921d534ca9502d
d3vv3 <jaimecondel@gmail.com>
2021-11-08 18:22:26 +0100

    fix: use nif_nie as key since its more viable

--- src/utilities/helpers.py
+++ src/utilities/helpers.py
@@ -8,18 +8,18 @@ def calculate_people_inside(accessed, exited):
     results = {}
     for entry in accessed:
         try:
-            current = results[entry.email]
+            current = results[entry.nif_nie]
         except Exception:
             current = 0
         current = current + 1
-        results[entry.email] = current
+        results[entry.nif_nie] = current
     for entry in exited:
         try:
-            current = results[entry.email]
+            current = results[entry.nif_nie]
         except Exception:
             current = 0
         current = current - 1
-        results[entry.email] = current
+        results[entry.nif_nie] = current
     return ", ".join([key for key, value in results.items() if value == 1])
 
 
